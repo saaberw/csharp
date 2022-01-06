ea1fbd8b51ca6ef768087aa5bd2d78cc570a8b07
saaberw <saaber.w@gmail.com>
2022-01-06 21:59:15 +0330

    Update staticExtensionProperty.cs

--- staticExtensionProperty.cs
+++ staticExtensionProperty.cs
@@ -1,13 +1,50 @@
-public static class IFrmDetailFunctions
+ public static class IFrmDetailFunctions
     {
-        private static DateTime modifiedTimeVal { get; set; }
-        public static DateTime getModifiedTime(this IFrmDetail frm)
+        private static DateTime modifiedTime{ get; set; }
+        public static DateTime getModifiedTime(this IDetailViewForm frm)
         {
-            return modifiedTimeVal;
+            return modifiedTime;
         }        
-        public static void setModifiedTime(this IFrmDetail frm,DateTime modifiedTime)
+        public static void setModifiedTime(this IDetailViewForm frm,DateTime time)
         {
-            modifiedTimeVal=modifiedTime;
+            modifiedTime=time; 
         }
 
     }
+   
+    public interface IListViewForm
+    {
+        Type getDetailFormType { get; set; }
+        DateTime listModifiedTime { get; set; }
+        DateTime listRefreshedTime { get; set; }
+
+        void refreshList();
+    }
+
+    public interface IDetailViewForm
+    {
+
+    }
+
+//...
+    
+//-----------------------[ set value for static (singleton) prop ]------------------------------------
+if (frm is IDetailViewForm)
+if(frm.parentFrmList != null)
+     (this as IDetailViewForm).setModifiedTime( DateTime.Now);
+
+
+//-----------------------[ get value from static prop ]------------------------------------
+if (frm is IListViewForm)
+{
+    IListViewForm parentForm = frm as IListViewForm;
+    Type childForm =(frm as IListViewForm).getDetailFormType;
+    if (childForm is IDetailViewForm)
+    {
+        DateTime modifiedTime=(childForm as IDetailViewForm).getModifiedTime();
+        if (parentForm.listRefreshedTime < modifiedTime)
+            parentForm.refreshList();
+    }
+}
+
+
