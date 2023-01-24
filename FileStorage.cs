70dd9d5dccc6fbd623095f7ec65098f32f4e1bda
saaberw <saaber.w@gmail.com>
2023-01-24 09:58:08 +0330

    Update FileStorage.cs

--- FileStorage.cs
+++ FileStorage.cs
@@ -1,4 +1,14 @@
  
+//In .NET, a string (System.String) can contain an initial UTF-8 Byte Order Mark (BOM) which might not be seen in ordinary processing but is present when converted to a character array or into an encoding byte array.
+//
+//For example, a text file might be saved in UTF8 format with UTF-8 Byte Order Mark bytes at the start, ie 0xEF 0xBB 0xBF. You might receive this file in ASP.NET using a FileUpload control, or read it directly in a Forms .NET app in C#:
+//
+//byte[] FileBytes = File.ReadAllBytes(path);
+//string content = Encoding.UTF8.GetString(FileBytes);
+//
+//
+//If the file contains these 7 bytes (in hex) EF BB BF 44 65 61 72 then content will superficially contain the single word "Dear", eg as seen in the debugger, and content.StartsWith("Dear") will return true.
+
  /// ## Read file content as byte arrays via stream
  public List<string> ReadFileContentToList(string filename)
     {
